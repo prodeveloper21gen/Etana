dc7e800adfbcc9451a03a9f794a8451d5550adb4
Muhammad <sadriddinzoda81@gmail.com>
2024-10-29 18:13:10 +0500

    add

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,9 +18,9 @@ const Footer = () => {
         </Box>
         <img data-aos='fade-right' src="/src/assets/svg/etend.svg" style={{ maxWidth: '100%', filter: 'drop-shadow(0 0 60px #462a2d)', height: 'auto', bottom: '0', position: 'absolute', zIndex: '0' }} alt="" />
         <img src="/src/assets/img/image copy 28.png" style={{ maxWidth: '100%', filter: 'drop-shadow(0 0 60px #3a175d)', height: 'auto', bottom: '0', position: 'absolute', zIndex: '10' }} alt="" />
-        <img src="/src/assets/svg/down.svg" style={{ maxWidth: '100%', height: 'auto', bottom: '0', position: 'absolute', zIndex: '50' }} alt="" />
+        <img src="/src/assets/svg/down.png" style={{ maxWidth: '100%', height: 'auto', bottom: '0', position: 'absolute', zIndex: '50' }} alt="" />
         <Link to="header" smooth={true} duration={1500}>
-          <img className='upAndDown' src="/src/assets/img/image copy 29.png" style={{ maxWidth: '30%', height: 'auto', bottom: '20px', position: 'absolute', zIndex: '50' }} alt="" />
+          <img className='upAndDown' src="/src/assets/img/image copy 29.png" style={{ maxWidth: '30%', height: 'auto', bottom: '20px', position: 'absolute', zIndex: '20' }} alt="" />
         </Link>
         <img className='headDown' src="/src/assets/img/image copy 30.png" style={{ maxWidth: '30%', height: 'auto', bottom: '10px', right: '0', position: 'absolute', zIndex: '10' }} alt="" />
         <Box>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,9 @@
 import { Box } from "@mui/material";
 import HeroSection from "../HeroSection/HeroSection.jsx";
-import { Link } from 'react-scroll';
 
 const Header = () => {
   return (<Box sx={{ background: 'linear-gradient(145deg, #aadae7, #56aac7)', }}>
-    <Link to="section2" smooth={true} duration={1000}>
       <HeroSection />
-    </Link>
   </Box>)
 }
 

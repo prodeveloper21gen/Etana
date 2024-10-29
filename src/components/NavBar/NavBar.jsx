becd52fe4f2821d40d007da2c6e1cb0b61b87ddc
Muhammad <sadriddinzoda81@gmail.com>
2024-10-29 17:02:13 +0500

    edit /src/ imports

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
         <Element name='header'>
             <Box sx={{ padding: '20px', display: 'flex', justifyContent: 'space-between' }}>
-                <Box><img src="/src/assets/svg/etenaLogo.svg" alt="" /></Box>
+                <Box><img src="src/assets/svg/etenaLogo.svg" alt="" /></Box>
                 <Box sx={{ display: 'flex', gap: '10px', height: 'fit-content', zIndex: '50' }}>
                     <Button sx={{ color: 'white', borderRadius: '30px', backgroundColor: '#ffffff50' }}><img src="/src/assets/svg/diskord.svg" style={{ aspectRatio: '1' }} alt="" /></Button>
                     <Button sx={{ color: 'white', borderRadius: '30px', backgroundColor: '#ffffff50' }}><X /></Button>

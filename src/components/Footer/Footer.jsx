d9a8e2f625bcefd3098f4eba302399759d87ddd1
Muhammad <sadriddinzoda81@gmail.com>
2024-10-29 17:31:16 +0500

    edit imports

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,13 +16,13 @@ const Footer = () => {
           <Typography data-aos='zoom-in-up' variant="h3" sx={{ fontFamily: 'Kaushan Script', fontSize: { md: '40px', xs: '4vw' }, color: 'white' }}>Begin Your Adventure <br /> in the World of Etena.</Typography>
           <Button sx={{background:'linear-gradient(-145deg, #ff4b32, #fe7e24)', color:'white', fontSize:'17px', padding:'12px', borderRadius:'30px'}}>Inferno magic travel</Button>
         </Box>
-        <img data-aos='fade-right' src="src/assets/svg/etend.svg" style={{ maxWidth: '100%', filter: 'drop-shadow(0 0 60px #462a2d)', height: 'auto', bottom: '0', position: 'absolute', zIndex: '0' }} alt="" />
-        <img src="src/assets/img/image copy 28.png" style={{ maxWidth: '100%', filter: 'drop-shadow(0 0 60px #3a175d)', height: 'auto', bottom: '0', position: 'absolute', zIndex: '10' }} alt="" />
-        <img src="src/assets/svg/down.svg" style={{ maxWidth: '100%', height: 'auto', bottom: '0', position: 'absolute', zIndex: '50' }} alt="" />
+        <img data-aos='fade-right' src="/assets/svg/etend.svg" style={{ maxWidth: '100%', filter: 'drop-shadow(0 0 60px #462a2d)', height: 'auto', bottom: '0', position: 'absolute', zIndex: '0' }} alt="" />
+        <img src="/assets/img/image copy 28.png" style={{ maxWidth: '100%', filter: 'drop-shadow(0 0 60px #3a175d)', height: 'auto', bottom: '0', position: 'absolute', zIndex: '10' }} alt="" />
+        <img src="/assets/svg/down.svg" style={{ maxWidth: '100%', height: 'auto', bottom: '0', position: 'absolute', zIndex: '50' }} alt="" />
         <Link to="header" smooth={true} duration={1500}>
-          <img className='upAndDown' src="src/assets/img/image copy 29.png" style={{ maxWidth: '30%', height: 'auto', bottom: '20px', position: 'absolute', zIndex: '50' }} alt="" />
+          <img className='upAndDown' src="/assets/img/image copy 29.png" style={{ maxWidth: '30%', height: 'auto', bottom: '20px', position: 'absolute', zIndex: '50' }} alt="" />
         </Link>
-        <img className='headDown' src="src/assets/img/image copy 30.png" style={{ maxWidth: '30%', height: 'auto', bottom: '10px', right: '0', position: 'absolute', zIndex: '10' }} alt="" />
+        <img className='headDown' src="/assets/img/image copy 30.png" style={{ maxWidth: '30%', height: 'auto', bottom: '10px', right: '0', position: 'absolute', zIndex: '10' }} alt="" />
         <Box>
         </Box>
       </Box>
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -8,21 +8,22 @@ const HeroSection = () => {
     useEffect(() => {
         Aos.init({
             duration: 1000,
+
         });
     }, [Aos]);
-    return (<Box className="bacPosCenter" sx={{ position: 'relative', backgroundSize: 'contain', backgroundImage:'url("src/assets/img/image copy 17.png")', backgroundRepeat: 'no-repeat' }}>
+    return (<Box className="bacPosCenter" sx={{ position: 'relative', backgroundSize: 'contain', backgroundImage:'url("/assets/img/image copy 17.png")', backgroundRepeat: 'no-repeat' }}>
 
         <NavBar />
         <Box sx={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
-            <img className="textAnimation" src="src/assets/svg/etenaText.svg" style={{ maxWidth: '100%', height: 'auto' }} alt="" />
+            <img className="textAnimation" src="/assets/svg/etenaText.svg" style={{ maxWidth: '100%', height: 'auto' }} alt="" />
         </Box>
-        <img className="birdFlight" src="src/assets/img/image copy 4.png" style={{ position: 'absolute', zIndex: '1', maxWidth: '30%' }} alt="" />
-        <img className="upAndDown" src="src/assets/img/image copy 3.png" style={{ position: 'absolute', right: '50px', animationDuration: '5s', zIndex: '10', maxWidth: '15%' }} alt="" />
+        <img className="birdFlight" src="/assets/img/image copy 4.png" style={{ position: 'absolute', zIndex: '1', maxWidth: '30%' }} alt="" />
+        <img className="upAndDown" src="/assets/img/image copy 3.png" style={{ position: 'absolute', right: '50px', animationDuration: '5s', zIndex: '10', maxWidth: '15%' }} alt="" />
         <Box data-aos="fade-up" className="upAndDown" style={{ display: 'flex', filter: 'drop-shadow(0 0 10vw #77f)', padding: '20px', justifyContent: 'center', zIndex: '0', position: 'relative', top: '-20vw' }}>
-            <img src="src/assets/img/image copy 5.png" style={{ maxWidth: '100%', position: 'absolute', height: 'auto' }} alt="" />
+            <img src="/assets/img/image copy 5.png" style={{ maxWidth: '100%', position: 'absolute', height: 'auto' }} alt="" />
         </Box>
         <Box data-aos="fade-up" className="upAndDown" style={{ zIndex: '100', marginTop: '', animationDuration: '4s' }}>
-            <img src="src/assets/img/1-2 Mountains devider.png" style={{ maxWidth: '100%', filter:'drop-shadow(0 40px 30px black)', zIndex: '40', height: 'auto' }} alt="" />
+            <img src="/assets/img/1-2 Mountains devider.png" style={{ maxWidth: '100%', filter:'drop-shadow(0 40px 30px black)', zIndex: '40', height: 'auto' }} alt="" />
         </Box>
     </Box>)
 }
